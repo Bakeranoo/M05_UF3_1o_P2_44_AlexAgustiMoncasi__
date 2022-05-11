04fbb2d137d8369ca92c2e89c7379766bb24e7cb
Alex Agustí Moncasi <alexagusti00@gmail.com>
2022-05-11 23:03:29 +0200

    Desarrollo web

--- App_Code/Model/Product.cs
+++ App_Code/Model/Product.cs
@@ -9,7 +9,7 @@ namespace M05_UF3_P2_Template.App_Code.Model
     public class Product
     {
         public int Id { get; set; }
-        public enum TYPE { GAME, VIDEO }
+        public enum TYPE { GAME, VIDEO, NONE }
         public TYPE Type { get; set; }
         public string Summary { get; set; }
         public string Icon { get; set; }
@@ -19,7 +19,9 @@ namespace M05_UF3_P2_Template.App_Code.Model
         public DateTime Publishing { get; set; }
         public float Size { get; set; }
         public int Developer_Id { get; set; }
+        public Company Developer { get; set; }
         public int Editor_Id { get; set; }
+        public Company Editor { get; set; }
 
         public Product()
         {
@@ -27,6 +29,12 @@ namespace M05_UF3_P2_Template.App_Code.Model
         }
 
         public Product(DataRow row)
+        {
+            Fill(row);
+        }
+        public Product(int Id) : this(DatabaseManager.Select("Product", null, "Id = " + Id + " ").Rows[0]) { }
+
+        public void Fill(DataRow row)
         {
             //Id
             try
@@ -40,7 +48,7 @@ namespace M05_UF3_P2_Template.App_Code.Model
             //TYPE
             try
             {
-                Type = (TYPE)(int)row[1];
+                Type = (TYPE)int.Parse(row[1].ToString());
             }
             catch
             {
@@ -87,6 +95,10 @@ namespace M05_UF3_P2_Template.App_Code.Model
             {
                 Developer_Id = 0;
             }
+            if (Developer_Id > 0)
+            {
+                Developer = new Company(Developer_Id);
+            }
             //Editor
             try
             {
@@ -96,9 +108,11 @@ namespace M05_UF3_P2_Template.App_Code.Model
             {
                 Editor_Id = 0;
             }
+            if (Editor_Id > 0)
+            {
+                Editor = new Company(Editor_Id);
+            }
         }
-        public Product(int Id) : this(DatabaseManager.Select("Product", null, "Id = " + Id + " ").Rows[0]) { }
-
         public bool Update()
         {
             DatabaseManager.DB_Field[] fields = new DatabaseManager.DB_Field[]
@@ -115,6 +129,32 @@ namespace M05_UF3_P2_Template.App_Code.Model
                 new DatabaseManager.DB_Field("Editor", Editor_Id)
             };
             return DatabaseManager.Update("Product", fields, "Id = " + Id + " ") > 0 ? true : false;
+        }
+
+        public bool Add()
+        {
+            DatabaseManager.DB_Field[] fields = new DatabaseManager.DB_Field[]
+            {
+                new DatabaseManager.DB_Field("Type", (int)Type),
+                new DatabaseManager.DB_Field("Summary", Summary),
+                new DatabaseManager.DB_Field("Icon", Icon),
+                new DatabaseManager.DB_Field("Banner", Banner),
+                new DatabaseManager.DB_Field("Trailer", Trailer),
+                new DatabaseManager.DB_Field("Price", Price),
+                new DatabaseManager.DB_Field("Publishing", Publishing),
+                new DatabaseManager.DB_Field("Size", Size),
+                new DatabaseManager.DB_Field("Developer", Developer_Id),
+                new DatabaseManager.DB_Field("Editor", Editor_Id)
+            };
+            return DatabaseManager.Insert("Product", fields) > 0 ? true : false;
+        }
+        public bool Remove()
+        {
+            return Remove(Id);
+        }
+        public static bool Remove(int id)
+        {
+            return DatabaseManager.Delete("Product", id) > 0 ? true : false;
         }
     }
 }
